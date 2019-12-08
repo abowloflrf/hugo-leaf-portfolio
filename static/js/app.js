13718720b1fcc074063dd4a52a85b9744ccf3ecb
Ruofeng Lei <ruofenglei@outlook.com>
2019-12-08 11:43:45 +0800

    首页卡片动画实现 jquery

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,36 @@
 // 过滤卡片
 function filterCard() {
     var selected = $(".site-tab-selected")
-    if (selected.length < 1)
-        return
-    selectedTag = selected.text()
-    console.log(selectedTag)
+    selectedTag = selected.text().trim()
     var cards = $("article.post-card")
+    // 遍历所有的cards
     for (let index = 0; index < cards.length; index++) {
         const card = cards[index];
+        // 若一个tag都没有被选中则显示所有tab，跳过筛选card的逻辑
+        if (selected.length == 0) {
+            $(card).show({
+                duration: 600
+            })
+            continue
+        }
+        // 找到此card下的所有keywords，在card下有一个li列表
         var tags = $(card).find("li")
         var hide = true
         for (let j = 0; j < tags.length; j++) {
             const tag = tags[j];
-            if (tag.innerText == selectedTag) {
+            if (tag.innerText.trim() == selectedTag) {
                 hide = false
                 break
             }
         }
         if (hide) {
-            $(card).hide()
+            $(card).hide({
+                duration: 600,
+            })
         } else {
-            $(card).show()
+            $(card).show({
+                duration: 600,
+            })
         }
     }
 }
@@ -77,15 +87,19 @@ $(function () {
         })
     })
 
-    // 首页tab实现
+    // 监听tab点击事件
     $(".site-tab").on("click", event => {
         var tab = $(event.target)
         // tab样式的变更
         if (!tab.hasClass("site-tab-selected")) {
+            // 若tab本身未被选中则先移除所有tab的selected class，再给点击的目标tab加上class
             $(".site-tab").removeClass("site-tab-selected")
             tab.addClass("site-tab-selected")
-            // 卡片的过滤和筛选
-            filterCard()
+        } else {
+            // 若tab本身已经被选中了则移除其selected class
+            tab.removeClass("site-tab-selected")
         }
+        // 卡片的过滤和筛选
+        filterCard()
     })
 });
