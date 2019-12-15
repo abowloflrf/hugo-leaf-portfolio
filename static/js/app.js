d144975e92bf5f30c78af49328bd3c78e3742905
Ruofeng Lei <ruofenglei@outlook.com>
2019-12-15 21:49:04 +0300

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,16 @@
 // 过滤卡片
 function filterCard() {
     var selected = $(".site-tab-selected")
-    selectedTag = selected.text().trim()
+    selectedTag = selected.text().trim() // trim 用于删除目标字符串前后的空格等空白字符
     var cards = $("article.post-card")
+    var transitionTime = 400 // 卡片变化的过渡时间，单位ms
     // 遍历所有的cards
     for (let index = 0; index < cards.length; index++) {
         const card = cards[index];
-        // 若一个tag都没有被选中则显示所有tab，跳过筛选card的逻辑
+        // 若一个tag都没有被选中则显示所有tab，跳过筛选card的逻辑，直接将所有的card设置为可见
         if (selected.length == 0) {
             $(card).show({
-                duration: 600
+                duration: transitionTime
             })
             continue
         }
@@ -18,24 +19,59 @@ function filterCard() {
         var hide = true
         for (let j = 0; j < tags.length; j++) {
             const tag = tags[j];
+            // 至少有一个匹配则将这个card设置为可见，即hide=false
             if (tag.innerText.trim() == selectedTag) {
                 hide = false
                 break
             }
         }
+        // 设置card的可见属性
         if (hide) {
             $(card).hide({
-                duration: 600,
+                duration: transitionTime
             })
         } else {
             $(card).show({
-                duration: 600,
+                duration: transitionTime
             })
         }
     }
 }
 
+// 判断元素是否在浏览器可视范围内
+function isInViewport(elem) {
+    var top_of_element = elem.offset().top;
+    var bottom_of_element = elem.offset().top + elem.outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+        return true
+    }
+    return false
+};
+
+// 将指定selector匹配的元素加上或删除class
+function toggleClassForShow(selector, className) {
+    $(selector).each(function (i, elem) {
+        var e = $(elem);
+        if (isInViewport(e)) {
+            e.addClass(className);
+        } else {
+            e.removeClass(className)
+        }
+    });
+}
+
 $(function () {
+    toggleClassForShow("img.show-animation", "animated fadeIn");
+    toggleClassForShow("span.markpen", "markpen-show");
+
+    $(window).on("scroll",function () {
+        toggleClassForShow("img.show-animation", "animated fadeIn");
+        toggleClassForShow("span.markpen", "markpen-show");
+    });
+
     // 点击横向滚动实现
     var scrollBtns = document.querySelectorAll(".horizontal-scroll-btn")
     scrollBtns.forEach(btn => {
